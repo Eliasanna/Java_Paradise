9eab82fba5753a00123093701f8527edf5ef66c8
Patrick Brouille <patrick@brouille.net>
2022-02-11 15:31:32 +0100

    feature: added env file to store user credentials to db

--- src/com/intiformation/app/util/ConnectionManager.java
+++ src/com/intiformation/app/util/ConnectionManager.java
@@ -1,17 +1,39 @@
 package com.intiformation.app.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
     private static Connection connection;
-    private static String url = "jdbc:mysql://localhost:3306/java_paradise";
-    private static String user = "root";
-    private static String password = "";
+    private static String url ;
+    private static String user;
+    private static String password;
 
     public static Connection getConnection() {
         if(connection==null ){
+            try (InputStream input = new FileInputStream(".env")) {
+
+                Properties prop = new Properties();
+                // load a properties file
+                prop.load(input);
+                // get the property value and print it out
+                url = prop.getProperty("db.url");
+                user = prop.getProperty("db.user");
+                password = prop.getProperty("db.password");
+
+//                System.out.println(prop.getProperty("db.url"));
+//                System.out.println(prop.getProperty("db.user"));
+//                System.out.println(prop.getProperty("db.password"));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             try {
                  connection = DriverManager.getConnection(url, user, password);
             } catch (SQLException e) {
